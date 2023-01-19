0319bcf2e12c02423acecf89d66645ca085939f8
IgnacioSanchezLittau <sanchezlittauignacio@gmail.com>
2023-01-19 13:51:01 -0300

    inicio de la segunda pre entrega

---
+++ app.js
@@ -0,0 +1,21 @@
+
+class Bebidas {
+    constructor(nombre, precio, marca){
+    this.nombre = nombre;
+    this.preio = precio;
+    this.marca = marca;
+    }
+
+
+}
+
+const jugadores = []
+
+let Bebida1 = new Bebidas("Fernet", 1000, "Branca")
+let Bebida2 = new Bebidas("Ron", 1700, "Havana")
+let Bebida3 = new Bebidas("Gin", 1600, "Merle")
+let Bebida4 = new Bebidas("Campari", 1300, "Campari")
+let Bebida5 = new Bebidas("Cerveza", 800, "Heineken")
+let Bebida6 = new Bebidas("Champagne", 4000, "Chandon")
+
+console.log(jugadores)
