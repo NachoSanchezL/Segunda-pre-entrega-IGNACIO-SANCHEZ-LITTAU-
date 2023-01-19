4e5b7f9ff55dc70361ec5f5be8f2a4f067245cb7
IgnacioSanchezLittau <sanchezlittauignacio@gmail.com>
2023-01-19 14:47:03 -0300

    Fin de la segunda entrega

--- app.js
+++ app.js
@@ -2,20 +2,41 @@
 class Bebidas {
     constructor(nombre, precio, marca){
     this.nombre = nombre;
-    this.preio = precio;
+    this.precio = precio;
     this.marca = marca;
     }
 
 
 }
 
-const jugadores = []
+const bebidas = []
 
-let Bebida1 = new Bebidas("Fernet", 1000, "Branca")
-let Bebida2 = new Bebidas("Ron", 1700, "Havana")
-let Bebida3 = new Bebidas("Gin", 1600, "Merle")
-let Bebida4 = new Bebidas("Campari", 1300, "Campari")
-let Bebida5 = new Bebidas("Cerveza", 800, "Heineken")
-let Bebida6 = new Bebidas("Champagne", 4000, "Chandon")
+bebidas.push(new Bebidas("Fernet", 1000, "Branca"))
+bebidas.push(new Bebidas("Ron", 1700, "Havana"))
+bebidas.push(new Bebidas("Gin", 1600, "Merle"))
+bebidas.push(new Bebidas("Campari", 1300, "Campari"))
+bebidas.push(new Bebidas("Cerveza", 800, "Heineken"))
+bebidas.push(new Bebidas("Champagne", 4000, "Chandon"))
 
-console.log(jugadores)
+console.log(bebidas)
+
+let nuevaBebida = 0 
+while (nuevaBebida < 3) {
+
+let pregunta = new Bebidas(prompt("Ingresa la nueva bebida"),
+        Number(prompt("Ingresa el precio")),
+        (prompt("Ingresa la marca")))
+        bebidas.push(pregunta)
+
+    nuevaBebida ++
+
+}
+
+console.log(bebidas)
+
+
+
+function filtrarPorPrecio(precio){
+        return bebidas.filter(propiedad => propiedad.precio <= Number(precio))
+}
+        console.log(filtrarPorPrecio(1200))
